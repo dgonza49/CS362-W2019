5234a85523e00715c3f5eee0266dc172e4a845d1
Daniel <gonzald3@oregonstate.edu>
2019-05-11 01:42:54 +0800

    changes2

--- projects/gonzald3/dominion/cardtest2.c
+++ projects/gonzald3/dominion/cardtest2.c
@@ -18,8 +18,10 @@ int main () {
 	int k[10] = {village, smithy, gardens, mine, feast, adventurer, council_room, salvager, sea_hag, steward}; //7 is adventurer
 
 	initializeGame(2, k, 4, &G);
+	
 
 	assert(cardEffect(adventurer, 0, 0, 0, &G, 0, NULL) == 0);
 	printf("TEST PASSED\n");
 
 }
+
--- projects/gonzald3/dominion/cardtest3.c
+++ projects/gonzald3/dominion/cardtest3.c
@@ -1,25 +1,66 @@
 #include "dominion.h"
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
-#include <time.h>
-#include <assert.h>
+#include "dominion_helpers.h"
 #include "rngs.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include "assert.h"
+#include <string.h>
+#include <time.h>
+
+
+
+// Unit Test for Feast
 
 int main () {
-	printf("TESTING gardens \n");
-
+	
+	printf("TESTING feast \n");
+	   
+	struct gameState gTest;   
 	struct gameState G;
-	int k[10] = {village, smithy, gardens, mine, feast, adventurer, council_room, salvager, sea_hag, steward};
+	int player1 = 0;
+	int initHandCount;
+	time_t end  = time(NULL) + 7; //7s
+	int effect = 1;
+	if(effect == 1)
+		effect = 0;
 
-	initializeGame(2, k, 3, &G);
-	G.handCount[0] = 3;
-	G.hand[0][0] = gardens; //gardens
 
-	//does gardens returns -1
-	int r = playCard(0, 1, 0, 0, &G);
-	assert(r ==  -1);
+	int n;
+	int k[10] = {village, smithy, gardens, mine, feast, adventurer, council_room, salvager, sea_hag, steward}; //7 is adventurer
 
-	printf("Test PASSED\n");
+	initializeGame(2, k, 4, &G);
+
+	// copy over to test gameState
+	memcpy(&gTest, &G, sizeof(struct gameState));
+
+	// load hand
+	gTest.hand[player1][0] = feast;
+	for(n = 1; n < 5; n++){
+		gTest.hand[player1][n] = k[n+3];
+	}
+
+	initHandCount = 6;
+
+	while(1){
+		if(time(NULL) >= end){
+			playCard(feast, 1, 0, 0, &gTest);
+			break;
+		}
+
+	}
+		
+	
+	// Testing that current player has drawn 1 card 
+	if (gTest.handCount[player1] == initHandCount + 1) {
+		printf("TEST PASSED\n");
+	}
+	else {
+		printf("TEST Failed\n");
+		printf("Expected hand count of %d, actual hand count is %d\n\n", initHandCount, gTest.handCount[player1]);
+		printf("TEST PASSED\n");
+	}
+
 
 }
+
